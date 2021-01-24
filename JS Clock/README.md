51a07f10476fb055a5ea96354ef35f1d9a4f129e
badra022 <ahmedbadra29@gmail.com>
2021-01-24 08:35:20 +0200

    finished first project successfully!

--- JS Clock/README.md
+++ JS Clock/README.md
@@ -1,2 +1,10 @@
 # JAVASCRIPT CLOCK
 
+this is a my first project in this career(web development), it's a simple application which you can set your alarm and your event that it will be done in this alarm time, then the application will notify you when the time comes using some funny cats templates, there's also a button for party time.
+
+#### THIS APPLICATION IS FOR EDUCATIONAL PURPOSES ONLY!
+## practiced: 
+* html
+* css 
+* Javascript
+* bootstrap
--- JS Clock/script.js
+++ JS Clock/script.js
@@ -1,7 +1,7 @@
 setInterval( clockUpdate , 1000 );
 setInterval( clockNumbersUpdate , 1000);
 setInterval( updateAlarm , 1000);
-
+var party_flag = false;
 
 function clockUpdate() {
     let date = new Date();
@@ -39,34 +39,59 @@ function parseNumber(str) {
     }
 }
 function updateAlarm() {
-    if (inputMDEx1.value) {
-        console.log(parseNumber(inputMDEx1.value.split(':')[0]));
-        console.log(parseNumber(inputMDEx1.value.split(':')[1]));
-
-        let type = document.getElementById("inputState").value;
-        if(type == "Nap"){
-            document.getElementById("timeEvent").innerHTML = "Wake Up!";
+    if(!party_flag){
+        if (inputMDEx1.value) {
+            console.log(parseNumber(inputMDEx1.value.split(':')[0]));
+            console.log(parseNumber(inputMDEx1.value.split(':')[1]));
+    
+            let date = new Date();
+            let alarm_hour = inputMDEx1.value.split(':')[0];
+            let alarm_minute = inputMDEx1.value.split(':')[1];
+            let type = document.getElementById("inputState").value;
+            if (alarm_hour == date.getHours() && (date.getMinutes() - alarm_minute <= 5)) {
+                if(type == "Nap"){
+                    document.getElementById("alarm-img").src = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
+                    document.getElementById("timeEvent").innerHTML = "Let's take a Nap!";
+                }
+                else if(type == "Lunch"){
+                    document.getElementById("alarm-img").src = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
+                    document.getElementById("timeEvent").innerHTML = "LET'S EAT!";
+                }
+                else if(type == "WakeUp"){
+                    document.getElementById("alarm-img").src = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
+                    if(date.getHours() >= 5 && date.getHours() < 12)
+                    {
+                        document.getElementById("timeEvent").innerHTML = "Good Morning!";
+                    }
+                    else if(date.getHours() >= 12 && date.getHours() < 18)
+                    {
+                        document.getElementById("timeEvent").innerHTML = "Good afternoon!";
+                    }
+                    else
+                    {
+                        document.getElementById("timeEvent").innerHTML = "Good evening!";
+                    }
+                }
+            }
+            else {
+                document.getElementById("alarm-img").src = "https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop";
+                document.getElementById("timeEvent").innerHTML = "NO CURRENT ALARM!";
+            }
         }
-        else if(type == "Lunch"){
-            document.getElementById("timeEvent").innerHTML = "LET'S EAT!";
-        }
-        else if(type == "WakeUp"){
-            document.getElementById("timeEvent").innerHTML = "Good Morning!";
-            document.getElementById("timeEvent").innerHTML = "Good evening!";
-            document.getElementById("timeEvent").innerHTML = "Good afternoon!";
-        }
-        else {
-            document.getElementById("timeEvent").innerHTML = "NO CURRENT ALARM!";
-        }
-
     }
+
 }
 
 function party() {
-    let party = document.getElementById("party-button").value;
-    console.log(document.getElementById("party-button").value);
-
-    document.getElementById("timeEvent").innerHTML = "let's Party!";
+    if(party_flag) {
+        document.getElementById("alarm-img").src = "https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop";
+        document.getElementById("timeEvent").innerHTML = "NO CURRENT ALARM!";
+    }
+    else {
+        document.getElementById("alarm-img").src = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/partyTime.jpg";
+        document.getElementById("timeEvent").innerHTML = "let's Party!";
+    }
+    party_flag = !party_flag;
 }
 
 clockUpdate();
